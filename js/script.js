536a13aeec2de5146c6370f6822b9e634f475529
Jiwon Han <jiwon1.han@gmail.com>
2019-02-10 20:37:40 -0800

    finished

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-
-  $("h1").click(function() {
-    alert("This is a header.");
-  } );
-
-  $("p").click(function() {
-    alert("This is a paragragh.")
-  });
---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+  jQuery(".clickable").click(function() {
+    jQuery("#walrus-showing").toggle();
+    jQuery("#walrus-hidden").toggle();
+  });
+});
